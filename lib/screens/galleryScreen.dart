f297eade06ca4e415141d9faf8dd7ff243c396b6
Stefan-George Cojocaru <cojoc07@gmail.com>
2021-02-26 16:52:45 +0200

    fixed masonry list
    fixed masonry list

--- lib/screens/galleryScreen.dart
+++ lib/screens/galleryScreen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:transparent_image/transparent_image.dart';
 
 class GalleryScreen extends StatefulWidget {
   @override
@@ -22,10 +23,10 @@ class _GalleryScreenState extends State<GalleryScreen> {
 
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
+
       setState(
         () {
           _lista = data;
-          print(data[0]["title"]);
         },
       );
       _isLoading = false;
@@ -46,47 +47,66 @@ class _GalleryScreenState extends State<GalleryScreen> {
     String title;
     String thumbnailUrl;
 
-    return _isLoading
-        ? Center(
-            child: CircularProgressIndicator(
-            backgroundColor: Colors.orange,
-            strokeWidth: 8,
-          ))
-        : StaggeredGridView.countBuilder(
-            crossAxisCount: 4,
-            itemCount: 32,
-            itemBuilder: (BuildContext context, int index) => Container(
-              color: Colors.green,
-              child: Material(
-                child: InkWell(
-                  onTap: () {
-                    title = _lista[index]["title"];
-                    thumbnailUrl = _lista[index]["thumbnailUrl"];
-
-                    showBarModalBottomSheet(
-                      expand: true,
-                      context: context,
-                      backgroundColor: Colors.transparent,
-                      builder: (context) => Center(
-                        child: Column(
-                          children: [
-                            Text("Exemplu"),
-                            //Text("Elementul $index cu url $_lista[$index]")
-                          ],
-                        ),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white24,
+        floatingActionButton: FloatingActionButton(
+          // isExtended: true,
+          child: Icon(Icons.add),
+          backgroundColor: Colors.green,
+          onPressed: () {
+            setState(() {
+              List<dynamic> test = [
+                {
+                  "id": "42",
+                  "createdAt": "2021-02-26T09:22:48.418Z",
+                  "title": "transition",
+                  "image":
+                      "https://cdn.pixabay.com/photo/2016/04/04/15/30/girl-1307429_960_720.jpg",
+                  "description": "China"
+                }
+              ];
+              _lista.addAll(test);
+            });
+          },
+        ),
+        appBar: AppBar(
+          title: Text("Dynamic masonry grid"),
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+        ),
+        body: Container(
+          margin: EdgeInsets.all(12),
+          child: !_isLoading
+              ? StaggeredGridView.countBuilder(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 12,
+                  itemCount: _lista?.length,
+                  itemBuilder: (context, index) => Container(
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
                       ),
-                    );
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      child: FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: _lista[index]["image"],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  staggeredTileBuilder: (index) {
+                    return StaggeredTile.count(1, index.isEven ? 1.2 : 1.5);
+                    //return StaggeredTile.fit(1);
                   },
-                  child: FittedBox(
-                      child: Image.network(_lista[index]["thumbnailUrl"]),
-                      fit: BoxFit.cover),
-                ),
-              ),
-            ),
-            staggeredTileBuilder: (int index) =>
-                new StaggeredTile.count(2, index.isEven ? 2 : 1),
-            mainAxisSpacing: 4.0,
-            crossAxisSpacing: 4.0,
-          );
+                )
+              : null,
+        ),
+      ),
+    );
   }
 }
